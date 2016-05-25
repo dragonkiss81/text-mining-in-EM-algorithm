a2d9df9335ae926a1a2b53acd75c380cf7f2fa94
mropengate <dragonkiss81@gmail.com>
2016-05-25 21:25:45 +0800

    finish short term vec to long vec

--- src/scan.r
+++ src/scan.r
@@ -1,6 +1,8 @@
 library(SnowballC)
 library(tm)
 
+### set all file path
+setwd("~/Desktop/text-mining-in-EM-algorithm/test")
 txt <- c("Train/alt.atheism", 
          "Train/comp.graphics", 
          "Train/comp.os.ms-windows.misc" , 
@@ -21,9 +23,9 @@ txt <- c("Train/alt.atheism",
          "Train/talk.politics.misc" , 
          "Train/talk.religion.misc"
          )
-         
-tdm <- list()
 
+### generate term freq. in each topic
+tdm <- list()
 for(i in 1: length(txt)){
   my.corpus <- Corpus(DirSource(txt[i]), readerControl = list(language = "lat"))
   my.corpus <- tm_map(my.corpus, function(x) gsub("[^[:alnum:]]", " ", x))
@@ -31,19 +33,40 @@ for(i in 1: length(txt)){
   my.corpus <- tm_map(my.corpus, removePunctuation)
   my.corpus <- tm_map(my.corpus, removeNumbers)
   my.corpus <- tm_map(my.corpus, tolower)
-  # my.corpus <- tm_map(my.corpus, content_transformer(tolower))
+  # for some version : my.corpus <- tm_map(my.corpus, content_transformer(tolower))
   my.corpus <- tm_map(my.corpus, stemDocument, language = "english")  
   my.corpus <- tm_map(my.corpus, removeWords, stopwords("english"))
-  my.corpus <- Corpus(VectorSource(my.corpus))
+  my.corpus <- tm_map(my.corpus, PlainTextDocument)
+  # for some version need reset : my.corpus <- Corpus(VectorSource(my.corpus))
   
   df <- data.frame(as.list(apply(TermDocumentMatrix(my.corpus), 1, sum)))
   df
   tdm[[i]] <- df
 }
 
-# All term for EM analysis
+### All term for EM analysis
 all_term <- colnames(tdm[[1]])
 for(j in 2:length(txt)){
   all_term <- union(all_term, colnames(tdm[[j]]))
 }
+
+### corpus vector in each topic to all term vector
+ShortVectoLong <- function(long_term, short_vec){
+  loc <- which(long_term %in% colnames(short_vec))
+  loc_word <- long_term[loc]
+  
+  long_vec <- vector(mode="integer",length=length(long_term))
+  for(i in 1:length(loc)){
+    long_vec[loc[i]] <- as.numeric( short_vec[which(loc_word[i] == colnames(short_vec))] ) 
+  }
+  
+  long_vec
+}
+
+### generate document term matrix
+data.frame(matrix(NA, nrow = 2, ncol = 3))
+ShortVectoLong(all_term, tdm[[7]])
+
+
+
 
