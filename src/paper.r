57f5e865c3552cdde55ea9702c844af189f856c4
mropengate <dragonkiss81@gmail.com>
2016-05-31 12:53:50 +0800

    naive complete. del type conv, add labled count

--- src/paper.r
+++ src/paper.r
@@ -13,18 +13,21 @@ GetTermVector <- function(file_dir){
   my.corpus <- tm_map(my.corpus, removeWords, stopwords("english"))
   my.corpus <- tm_map(my.corpus, PlainTextDocument)
   # # for some version need reset : Corpus(VectorSource(my.corpus))
-  
-  return(data.frame(as.list(apply(TermDocumentMatrix(my.corpus), 1, sum))))
+
+  return(apply(TermDocumentMatrix(my.corpus), 1, sum))
 }
 
 ShortVectoLong <- function(long_term, short_vec){
-  loc <- which(long_term %in% colnames(short_vec))
+  loc <- which(long_term %in% names(short_vec))
   loc_word <- long_term[loc]
-  if(length(loc)==0) return(rep(1,length=length(long_term)))
+  
+  if(length(loc)==0){ 
+    return(rep(1,length=length(long_term)))
+  }
   
   long_vec <- vector(mode="integer",length=length(long_term))
   for(i in 1:length(loc)){
-    long_vec[loc[i]] <- as.numeric( short_vec[which(loc_word[i] == colnames(short_vec))] ) 
+    long_vec[loc[i]] <- as.numeric( short_vec[which(loc_word[i] == names(short_vec))] ) 
   }
   
   return(long_vec)
@@ -32,20 +35,28 @@ ShortVectoLong <- function(long_term, short_vec){
 
 ##### main #####
 
+all_time = Sys.time()
+
 ### set all file path
 setwd("~/Desktop/text-mining-in-EM-algorithm/test")
 catego <- list.dirs('Train', recursive=FALSE)
 
 ### generate term freq. in each topic
+used_label_count <- -1
 term_vec <- list()
 for(i in 1: length(catego)){
-  term_vec[[i]] <- GetTermVector(DirSource(catego[i]))
+  file <- DirSource(catego[i])
+  if(used_label_count!=-1){
+    file$filelist <- file$filelist[1:used_label_count]
+    file$length <- used_label_count
+  }
+  term_vec[[i]] <- GetTermVector(file)
 }
 
 ### generate All term set 
 all_term <- colnames(term_vec[[1]])
 for(i in 2:length(catego)){
-  all_term <- union(all_term, colnames(term_vec[[i]]))
+  all_term <- union(all_term, names(term_vec[[i]]))
 }
 
 ### generate term document matrix
@@ -59,8 +70,6 @@ for(i in 1:length(catego)){
 low_freq_term <- which(apply(tdm,1,sum)<=1)
 all_term <- all_term[-low_freq_term]
 tdm <- tdm[-low_freq_term,]
-
-
 
 ### calculate prob of word in a topic
 all_wordcount_in_a_topic <- apply(tdm,2,sum)
@@ -85,28 +94,25 @@ all_test <- paste0(rep('Test/',length(all_test)),
 
 ### Naive Algorithm
 ans_list <- vector(mode="character",length=length(all_test))
-names(ans_list) <- 1:length(all_test)
+# names(ans_list) <- 1:length(all_test)
 
-all_time = Sys.time()
 for(i in 1:length(all_test)){
-  
-  query_term <- data.frame(as.list(GetTermVectorCpp(all_test[i])))
+  query_term <- GetTermVectorCpp(all_test[i])
   query_term_long <- ShortVectoLong(all_term, query_term)
-
+  
   ### Using a Classifier
   log_for_product <- apply(word_in_a_class_prob, 2, function(x) sum(query_term_long * x))
   ans_list[i] <- catego[ which(log_for_product == max(log_for_product)) ]
   
-  print( paste(i, ans_list[i], collapse = " "))
+  # print( paste(i, ans_list[i], collapse = " "))
 }
-print(Sys.time() - all_time)
 
-setwd("~/Desktop/text-mining-in-EM-algorithm/")
-given_ans <- read.table("out/ans.test.txt")
+given_ans <- read.table("ans.test.txt")
 given_ans <- as.vector(t(given_ans[2]))
 
-sum(given_ans[1:2000] == paste0(lapply(ans_list[1:2000], function(x)substring(x,7,40)))) / 2000
+sum(given_ans == paste0(lapply(ans_list, function(x)substring(x,7,40)))) / 9419
 
+print(Sys.time() - all_time)
 
 
 
